4e381f704045aa8339c5c888bad1bc934f5b5a2d
Eliza de Jager <eliza@intellectionsoftware.com>
2016-06-13 19:17:08 +0200

    controller specs

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -4,12 +4,18 @@ class OwnersController < ApplicationController
 
   def create
     @owner = Owner.new(params[:owner])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @owner.to_json }
+    end
   end
 
   def update
   end
 
   def show
+    redirect_to owners_index_path
   end
 
   def delete
--- spec/controllers/owners_controller_spec.rb
+++ spec/controllers/owners_controller_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe OwnersController, :type => :controller do
 
+  render_views
+
 	it "should assign owner" do
-    post :create, owner: { name: "bob" } # format: :json
-    expect(assigns[:owner].name).to eq "bob"
+  	post :create, owner: { name: "bob" } # format: :json
+  	expect(assigns[:owner].name).to eq "bob"
 	end
 
+  it "show should redirect to index" do
+    get :show
+    expect(response).to redirect_to(owners_index_path)
+  end
+
+  it "show render index" do
+    get :index
+    expect(response).to have_http_status(:ok)
+    expect(response.status).to eq(200)
+    expect(response).to render_template(:index)
+  end  
 end
